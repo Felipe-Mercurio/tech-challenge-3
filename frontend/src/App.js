8fbde635bbf52bf957db6796df80422e750c40f3
Felipe-Mercurio <95919509+Felipe-Mercurio@users.noreply.github.com>
2025-05-28 01:40:27 -0300

    Atualização Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { AuthProvider } from './contexts/AuthContext';
+
+import Home from './pages/Home';
+import PostDetail from './pages/PostDetail';
+import CreatePost from './pages/CreatePost';
+import EditPost from './pages/EditPost';
+import Admin from './pages/Admin';
+import Login from './pages/Login';
+
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetail />} />
+          <Route
+            path="/posts/create"
+            element={
+              <PrivateRoute>
+                <CreatePost />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/posts/edit/:id"
+            element={
+              <PrivateRoute>
+                <EditPost />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <Admin />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
