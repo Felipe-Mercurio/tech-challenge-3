5a6f76937c98d4a333c7a17a792ac6682622398b
Felipe-Mercurio <95919509+Felipe-Mercurio@users.noreply.github.com>
2025-05-28 22:55:25 -0300

    Correção no Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const postsRouter = require('./routes/posts');
-const app = express();
 const cors = require('cors');
 
-app.use(express.json());
-app.use('/posts', postsRouter);
-
+const app = express();
 
 app.use(cors({
   origin: ['http://localhost:3000', 'https://tech-challenge-3-topaz.vercel.app'], // front local e deploy
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
+
+app.use(express.json());
+
+// ✅ CORS já ativado aqui antes de usar as rotas
+app.use('/posts', postsRouter);
 
 module.exports = app;
